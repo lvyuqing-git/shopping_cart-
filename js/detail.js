55ce94bd3b7c410199701e57e1c2f59cdb791c99
lvyuqing <1944124354@qq.com>
2019-11-23 11:44:47 +0800

    将信息存储到本地仓库中部分完成

--- js/detail.js
+++ js/detail.js
@@ -1,11 +1,44 @@
 //获取list页面传递过来的参数，根据参数生成页面
 $(function () {
-    console.log(phoneData);
-    
+    let id = location.search.substr(4);
     let target = phoneData.find(function (e) {
-        return e.pID == location.search.substr(4)
+        return e.pID == id;
     });
-    $('.preview-img img').attr('src',target.imgSrc);
+    $('.preview-img img').attr('src', target.imgSrc);
     $('.summary-price em').text(target.price);
     $('.sku-name').text(target.name);
+
+    //////////////////----------------------------------------------
+
+
+
+    $('.addshopcar').click(() => {
+        let number = $('.choose-number').val();
+        if (isNaN(number) || number.trim().length == 0 || parseInt(number) <= 0) {
+            alert('请重新输入件数');
+            return;
+        }
+        let arr = kits.loadData('cartListData');
+        let exist = arr.find(function (e, i) {
+            return e.pID == id;
+        });
+        number = parseInt(number);
+        if (exist) {
+            exist.number += number;
+            exist.price = 10000;
+        } else {
+            let obj = {
+                pID: target.pID,
+                imgSrc: target.imgSrc,
+                name: target.name,
+                price: target.price,
+                number: number,
+            }
+            arr.push(obj);
+        }
+
+       
+        kits.saveData('cartListData', arr)
+        location.href = './cart.html';
+    });
 });
